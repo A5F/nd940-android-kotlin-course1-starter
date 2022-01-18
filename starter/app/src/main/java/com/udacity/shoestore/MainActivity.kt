102262a58d38aa68688707f1b80fad3252eddb07
Andrea <andreaferri46@gmail.com>
2022-01-18 16:58:59 +0100

    login complete

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,14 +3,16 @@ package com.udacity.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        activityMainBinding.toolbar.title = getString(R.string.app_name)
         Timber.plant(Timber.DebugTree())
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
@@ -16,6 +18,58 @@ class LoginFragment : Fragment() {
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false)
 
+        activity?.title = getString(R.string.app_name)
+        binding.btnLoginNew.setOnClickListener {
+            if (checkInputField(binding.etUsername.text.toString(), binding.etUsername.text.toString())) {
+                findNavController().navigate(R.id.action_login_to_welcome)
+            }else{
+                Toast.makeText(requireContext(), "DATI NON CORRETTI", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.btnLoginOld.setOnClickListener {
+            if (checkInputField(binding.etUsername.text.toString(), binding.etUsername.text.toString())) {
+                val responseCheckUser = checkUser(binding.etUsername.text.toString(), binding.etUsername.text.toString())
+                when (responseCheckUser) {
+                    "OK" -> {
+                        findNavController().navigate(R.id.action_login_to_welcome)
+                    }
+                    "NO_USER_FOUND" -> {
+                        Toast.makeText(requireContext(), "UTENTE NON TROVATO", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                    "INCORRECT_PASSWORD" -> {
+                        Toast.makeText(requireContext(), "PASSWORD NON CORRETTA", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "DATI NON CORRETTI", Toast.LENGTH_LONG).show()
+            }
+        }
         return binding.root
+    }
+
+    private fun checkInputField(username: String, password: String): Boolean{
+        return username.isNotEmpty() && password.isNotEmpty()
+    }
+
+    private fun checkUser(username: String, password: String): String{
+        val mockedUser= getMockedUser()
+        if (mockedUser.get(username)?.isNotEmpty() == true){
+            if (mockedUser.get(username).equals(password)){
+                return "OK"
+            }else{
+                return "INCORRECT_PASSWORD"
+            }
+        }else{
+            return "NO_USER_FOUND"
+        }
+    }
+
+    private fun getMockedUser(): HashMap<String, String>{
+        val users = hashMapOf<String, String>()
+        users.put("admin", "admin")
+        return users
     }
 }

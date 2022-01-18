0fbd85d105f8be31698b8dc2d4502e48afa756e1
Andrea <andreaferri46@gmail.com>
2022-01-18 17:15:49 +0100

    welcome complete

--- starter/app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -15,6 +16,13 @@ class WelcomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
         val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_welcome, container, false)
+
+        binding.welcomeTitle.text = getString(R.string.welcome_title)
+        binding.welcomeSubtitle.text = getString(R.string.welcome_subtitle)
+
+        binding.btnGoForward.setOnClickListener {
+            findNavController().navigate(R.id.action_welcome_to_instruction)
+        }
 
         return binding.root
     }
